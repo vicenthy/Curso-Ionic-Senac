c70486da663cf4157e47022382b7bb474e0c0f8e
Atila Augusto dos Santos <atila.sistemas@gmail.com>
2018-11-26 21:44:40 -0200

    final da aula

--- senac-food/src/pages/home/home.ts
+++ senac-food/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { RestauranteProvider } from '../../providers/restaurante/restaurante';
 
 @Component({
   selector: 'page-home',
@@ -7,7 +8,13 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  restaurantes = [];
+  constructor(public navCtrl: NavController,
+              public restauranteProvider: RestauranteProvider) {
+  this.restauranteProvider.getAll()
+  .then( (result:any) => {
+    this.restaurantes = result;
+  } ).catch( err => console.error(err));
 
   }
 

a52b89b431684e37df35ff06902560e59af9d756
Atila Augusto dos Santos <atila.sistemas@gmail.com>
2018-11-28 19:04:45 -0200

    restaurantes

--- senac-food/src/providers/restaurante/restaurante.ts
+++ senac-food/src/providers/restaurante/restaurante.ts
@@ -1,5 +1,5 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 
 /*
   Generated class for the RestauranteProvider provider.
@@ -7,43 +7,48 @@ import { Injectable } from '@angular/core';
   See https://angular.io/guide/dependency-injection for more info on providers
   and Angular DI.
 */
-export const API_URL = 'http://192.168.43.190:3000';
+export const API_URL = "http://localhost:3000";
 
 @Injectable()
 export class RestauranteProvider {
+  constructor(public http: HttpClient) {}
 
-  constructor(public http: HttpClient) {
+  public getAll() {
+    return new Promise((resolve, reject) => {
+      this.http.get(API_URL + "/restaurante").subscribe( (result:any) => {
+          resolve(result);
+        },
+        error => {
+          reject(error);
+        }
+      );
+    });
   }
 
-  public getAll(){
-    return new Promise( (resolver, reject) => {
-      this.http.get(API_URL + '/restaurante')
-            .subscribe( (result:any) => {
-              resolver(result);
-            }, error => {
-              reject(error);
-            } )
-    })
+  public getProdutosByRestaurante(restaurante:string){
+    return new Promise((resolve, reject) => {
+    this.http.get(API_URL + `/produto?restaurantId=${restaurante}`).subscribe( (result:any) => {
+      resolve(result);
+    },
+    error => {
+      reject(error);
+    }
+  );
+});
+
   }
 
   public getReviewByRestaurante(restaurante:string){
-    return new Promise( (resolver, reject) => {
-    this.http.get(API_URL + `/reviews?restaurantId=${restaurante}`)
-    .subscribe( (result:any) => {
-      resolver(result);
-    }, error => {
+    return new Promise((resolve, reject) => {
+    this.http.get(API_URL + `/reviews?restaurantId=${restaurante}`).subscribe( (result:any) => {
+      resolve(result);
+    },
+    error => {
       reject(error);
-    } )
-})
-  }
-  public getProdutoByRestaurante(restaurante:string){
-    return new Promise( (resolver, reject) => {
-      this.http.get(API_URL + `/produto?restaurantId=${restaurante}`)
-      .subscribe( (result:any) => {
-        resolver(result);
-      }, error => {
-        reject(error);
-      } )
+    }
+  );
+});
+
   }
 
 
